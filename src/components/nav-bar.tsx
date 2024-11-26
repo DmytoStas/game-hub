08b67aaa16e9569d416d99bb4e4a0d046a22ff62
Dmytro Stas <dima7stas777@gmail.com>
2024-11-25 20:44:12 -0500

    Implement searching

--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./color-mode-switch";
 import SearchInput from "./search-input";
 
-export default function NavBar() {
+type NavBarProps = {
+  onSearch: (searchText: string) => void;
+};
+
+export default function NavBar({ onSearch }: NavBarProps) {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize={"60px"} />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -23,6 +23,7 @@ const useGames = (gameQuery: GameQuery) =>
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
         ordering: gameQuery.sordOrder,
+        search: gameQuery.searchText,
       },
     },
     [gameQuery]
